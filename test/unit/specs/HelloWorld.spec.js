a37f9931a6a0a24ec6e955467c92877a760799a0
DESKTOP-F109P7R\loluz <zy409815681@qq.com>
2017-11-20 22:22:11 +0800

    redo test

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import HelloWorld from '@/components/HelloWorld'
+import App from '@/App.vue'
 
 describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
+  it('delete item', () => {
+    const Constructor = Vue.extend(App)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-    .to.equal('Welcome to Your Vue.js App')
+    vm.addTodo()
+    vm._watcher.run()
+    expect(vm.index).to.equal(1)
   })
 })
